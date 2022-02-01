8807c80b93f01f16e9ec2252c986d3ec8444da92
wwwkang8 <wwwkang8@gmail.com>
2022-02-02 00:24:45 +0900

    엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name="delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // Enum이 문자열로 되어야 나중에 상태값이 추가되더라도 혼란이 없다.
+
+}
